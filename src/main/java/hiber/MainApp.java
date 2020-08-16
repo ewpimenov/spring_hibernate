4b731f0692d92e02599a5a557d6a25751711544d
ewpimenov <sherlokprs@gmail.com>
2020-08-16 20:35:58 +0300

    git commit

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -16,17 +16,17 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
 
-        User user1 = new User("User2", "Lastname2", "user1@mail.ru");
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
         user1.setCar(new Car("Car1", 111));
         userService.add(user1);
 
-        User user2 = new User("User2", "Lastname2", "user1@mail.ru");
-        user2.setCar(new Car("Car1", 222));
-        userService.add(user1);
+        User user2 = new User("User1", "Lastname2", "user2@mail.ru");
+        user1.setCar(new Car("Car2", 222));
+        userService.add(user2);
 
-        User user3 = new User("User2", "Lastname2", "user1@mail.ru");
-        user3.setCar(new Car("Car1", 333));
-        userService.add(user1);
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        user3.setCar(new Car("Car3", 333));
+        userService.add(user3);
 
 
         List<User> users = userService.listUsers();
